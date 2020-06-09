35131858353348d54ccac4927aa329d0f07a4fed
CIDBerlin <anton@polyakov.im>
2020-06-09 03:24:11 +0300

    add modal icons

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', function() {
-
+    // Tabs.
+    
 const tabs = document.querySelectorAll('.tabheader__item'),
       tabsContent = document.querySelectorAll('.tabcontent'),
       tabsParent = document.querySelector('.tabheader__items');
@@ -39,104 +40,105 @@ const tabs = document.querySelectorAll('.tabheader__item'),
         }
     });
     
+    // Timer.
+    
+    const deadline = '2020-07-11';
+    
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+        
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+    
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+    
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+        
+        updateClock();
+        
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+            
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+            
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+              
+    }
+    setClock('.timer', deadline);
+    
+    // Modal
+    
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+    
+    function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+    //modal.classlist.toggle('show');
+        clearInterval(modalTimerId);
+    }
+    
+    function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+    }
+    
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+    
+    modalCloseBtn.addEventListener('click', closeModal);
+    
+    modal.addEventListener('click', (e) => {
+        if(e.target === modal) { 
+            closeModal();
+        }
+    });
+    
+    document.addEventListener('keydown', (e) => { // Закрытие на esc.
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+    const modalTimerId = setTimeout(openModal, 5000);
+    
+        function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) { // Yoffset это прокрученная часть, далее видимая часть на сайте без прокрутки >= размеру всей страницы.
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll); // Удаляем появление при прокрутке, если мы уже один раз сделали это.
+    }
+        }
+    
+    window.addEventListener('scroll', showModalByScroll); // Добавляем открытие окна по прокрутке.
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//    // Tabs
-//    
-//	let tabs = document.querySelectorAll('.tabheader__item'),
-//		tabsContent = document.querySelectorAll('.tabcontent'),
-//		tabsParent = document.querySelector('.tabheader__items');
-//
-//	function hideTabContent() {
-//        
-//        tabsContent.forEach(item => {
-//            item.classList.add('hide');
-//            item.classList.remove('show', 'fade');
-//        });
-//
-//        tabs.forEach(item => {
-//            item.classList.remove('tabheader__item_active');
-//        });
-//	}
-//
-//	function showTabContent(i = 0) {
-//        tabsContent[i].classList.add('show', 'fade');
-//        tabsContent[i].classList.remove('hide');
-//        tabs[i].classList.add('tabheader__item_active');
-//    }
-//    
-//    hideTabContent();
-//    showTabContent();
-//
-//	tabsParent.addEventListener('click', function(event) {
-//		const target = event.target;
-//		if(target && target.classList.contains('tabheader__item')) {
-//            tabs.forEach((item, i) => {
-//                if (target == item) {
-//                    hideTabContent();
-//                    showTabContent(i);
-//                }
-//            });
-//		}
-//	});
